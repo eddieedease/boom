2d9e520418ef86f2871685df0114bf250b9dbb09
Eddie <eddie@edease.nl>
2023-10-19 11:20:33 +0200

    menuOpen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'boom';
+
+
+  menuOpen = false;
+
+  mobileMenuOpen(){
+    this.menuOpen =! this.menuOpen;
+  }
+
 }
+
+
